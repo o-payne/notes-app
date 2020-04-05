6b0999e5145618a32b2b8f4fb70fe8255b70f188
Owen <owen.payne42@gmail.com>
2020-04-05 18:37:06 -0400

    added yargs

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 const getNotes = require('./notes')
+const yargs = require('yargs')
+
 const chalk = require('chalk')
 
-const command = process.argv[2]
+//Customize yargs version
+yargs.version('1.1.0')
 
-if(command === 'add') {
-   console.log('adding note')  
-} 
-else if (command === 'remove') {
-    console.log('removing note')
-}
+// add, remove, read, list
+
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('adding a new note')
+    }
+})
+
+// create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('removing note')
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: () => {
+        console.log('listing notes')
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('reading note')
+    }
+})
+
+
+
+
+console.log(yargs.argv)
